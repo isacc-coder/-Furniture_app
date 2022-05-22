ef1027888f8cb62b6719a5b43d6d06945f8cc6af
isacc-coder <isaccfirew12@gmail.com>
2022-05-22 10:31:29 -0400

    first commit

---
+++ lib/screens/product/products_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constants.dart';
+import 'components/body.dart';
+
+class ProductsScreen extends StatelessWidget {
+  const ProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      backgroundColor: kPrimaryColor,
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      centerTitle: false,
+      title: Text('Dashboard'),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/notification.svg"),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
